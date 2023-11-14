6ba70d6e272272fb03a4d4a507f3c87115ba159b
Dr. Dillinger <peter.dillinger@srh.de>
2023-11-14 09:38:25 +0100

    Initial commit

---
+++ src/Zahlenraten.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Zahlenraten {
+    public static void main(String[] args) {
+        Scanner eingabe = new Scanner(System.in);
+
+        int geheimeZahl = 745;
+
+        // TODO
+    }
+}
